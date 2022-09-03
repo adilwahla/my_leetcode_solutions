91d33a4df526c57167e5be3e7e04212515433b63
Adil Rehman <48839911+adilwahla@users.noreply.github.com>
2022-09-03 23:16:32 +0500

    Update TwoSum.java

--- LinearSearch/TwoSum.java
+++ LinearSearch/TwoSum.java
@@ -1,1 +1,18 @@
-
+public class TwoSum {
+    public int[] twoSum(int[] arr, int target) {
+        int start=0;int end=arr.length-1;int[] result={1,1};
+        while(start<end){
+            if(arr[start]+arr[end]==target){
+                result[0]+=start;
+                result[1]+=end;
+                return result;
+            }
+            if(arr[start]+arr[end]>target){
+                end--;
+            }else{
+            start++;    
+            }
+        }
+        return result;
+    }
+}
