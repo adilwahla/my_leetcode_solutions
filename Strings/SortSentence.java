e6cee797da8aa741902eabb66f35e8fe4e4187c5
Adil Rehman <48839911+adilwahla@users.noreply.github.com>
2022-09-19 00:06:50 +0500

    Create SortSentence.java

---
+++ Strings/SortSentence.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String sortSentence(String s) {
+        String[] words=s.split(" ");
+        String strChar="123456789";
+        char[] ch=strChar.toCharArray();
+        String os="";
+        for(int i=0;i<words.length;i++){
+            for(String w:words){
+                if(w.charAt(w.length()-1)==ch[i]){
+                    os+=w.substring(0,w.length()-1)+" ";
+                }
+            }
+        }
+        return os.strip();
+    }
+}
