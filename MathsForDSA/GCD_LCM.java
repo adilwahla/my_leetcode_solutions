285dd588c5582c954c77dbbfb01c3b8cd3dd6d07
Adil Rehman <48839911+adilwahla@users.noreply.github.com>
2022-11-02 22:02:26 +0500

    Create GCD_LCM.java

---
+++ MathsForDSA/GCD_LCM.java
@@ -0,0 +1,14 @@
+package MathsForDsa;
+
+public class GCD_LCM {
+    public static void main(String[] args) {
+       System.out.println( gcd(4,8));
+    }
+
+    private static int gcd(int a, int b) {
+        if(a==0){
+            return b;
+        }
+        return gcd(b%a, a);
+    }
+}
