9ea73fdd5e236e223bd01db64b2bff26e3223e98
Adil Rehman <48839911+adilwahla@users.noreply.github.com>
2022-11-14 01:10:01 +0500

    189. Rotate Array
    Given an array, rotate the array to the right by k steps, where k is non-negative.
    
     
    
    Example 1:
    
    Input: nums = [1,2,3,4,5,6,7], k = 3
    Output: [5,6,7,1,2,3,4]
    Explanation:
    rotate 1 steps to the right: [7,1,2,3,4,5,6]
    rotate 2 steps to the right: [6,7,1,2,3,4,5]
    rotate 3 steps to the right: [5,6,7,1,2,3,4]
    Example 2:
    
    Input: nums = [-1,-100,3,99], k = 2
    Output: [3,99,-1,-100]
    Explanation: 
    rotate 1 steps to the right: [99,-1,-100,3]
    rotate 2 steps to the right: [3,99,-1,-100]
     
    
    Constraints:
    
    1 <= nums.length <= 105
    -231 <= nums[i] <= 231 - 1
    0 <= k <= 105
     
    
    Follow up:
    
    Try to come up with as many solutions as you can. There are at least three different ways to solve this problem.
    Could you do it in-place with O(1) extra space?

--- MathsForDSA/RotateArrayKsteps.java
+++ MathsForDSA/RotateArrayKsteps.java
@@ -1,1 +1,21 @@
-
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int start=0; int end=nums.length-1;
+        //reverse fun
+       k%=nums.length;
+        reverse (nums, start,  end);
+     
+        reverse(nums, start,k-1);
+        reverse(nums, k,end);
+        
+      
+    }
+    public void reverse(int[] nums, int s, int e){
+      while(s<=e && nums.length >1 ){
+       int temp=nums[s];
+       nums[s]=nums[e];
+       nums[e]=temp;
+       s++;e--;
+      }
+    }
+}
