5b5a8d0a090475a160b46fa4de3232bb10b22472
Adil Rehman <48839911+adilwahla@users.noreply.github.com>
2022-11-25 22:23:25 +0500

    Create PrintNo.java

---
+++ Recurrsion/PrintNo.java
@@ -0,0 +1,17 @@
+package Recursion;
+
+public class PrintNo {
+     public static void main(String[] args) {
+   printNum(5);
+     }
+
+    private static void printNum(int n) {
+       if(n==0){
+        return ;
+       }
+  
+       printNum(n-1);
+     System.out.println(n);
+    }
+     
+} 
