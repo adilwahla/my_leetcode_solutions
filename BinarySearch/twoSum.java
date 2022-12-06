02b01b1f55232751a82403c4877cfa52d20d0d82
Adil Rehman <48839911+adilwahla@users.noreply.github.com>
2022-12-06 23:44:32 +0500

    Create twoSum.java

---
+++ BinarySearch/twoSum.java
@@ -0,0 +1,51 @@
+  public static  int[] twoSum(int[] nums, int target) {
+           int[] nums2=Arrays.copyOf(nums, nums.length);
+           Arrays.sort(nums2);
+           //find two sum as binary
+          int[] numbers= binarySearch(nums2,target);
+          int a =numbers[0]; int b=numbers[1];
+          int[] ans=new int[2];
+          for (int i = 0; i < nums.length; i++) {
+            if(a==nums[i]){
+             ans[0]=i; break;
+            }
+          }
+          if(a!=b){
+            for (int i = 0; i < nums.length; i++) {
+                if(b==nums[i]){
+                    ans[1]=i; break;
+                   }
+            }
+          }
+          else {
+           for (int i = 0; i < nums.length; i++) {
+            if(b==nums[i] && a!=ans[0]){
+                ans[1]=i;
+            }
+           }
+          }
+           return ans;
+    }
+    public static int[] binarySearch(int[] arr, int target) {
+        //stat,mid,end are main points of BS Algo
+        int start=0;
+        int end=arr.length-1;
+       int a=0; int b=0;
+        while(start<end){
+            int sum= arr[start]+arr[end];// int may go out of bound                   
+            if(sum<target){
+                // move right 
+                start++;
+            }
+            if(sum>target){
+                // move left
+                end--;
+            }
+            else {
+               a=arr[start];b=arr[end];
+             return new int[]{a,b};
+            }
+        }
+
+        return new int[]{0,0};
+    }
