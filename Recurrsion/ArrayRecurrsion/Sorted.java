40d8940149b095741c527d676f26600265d5c52b
Adil Rehman <48839911+adilwahla@users.noreply.github.com>
2022-12-13 22:02:21 +0500

    Create Sorted.java

---
+++ Recurrsion/ArrayRecurrsion/Sorted.java
@@ -0,0 +1,15 @@
+package Recursion.LECTURE3arrays;
+
+public class Sorted {
+     public static void main(String[] args) {
+        int[] arr={1,2,3,6,8,9};
+        System.out.println(sorted(arr, 0));
+     }
+     static boolean sorted(int[] arr, int index){
+        //base condition
+              if(index==arr.length-1){
+                return true;
+              }
+              return (arr[index]<arr[index+1])&& sorted(arr, ++index);
+     }
+}
