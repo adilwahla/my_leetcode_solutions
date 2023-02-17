628302ef05a0afb7f9757e45d4c4e8c9af713920
Adil Rehman <48839911+adilwahla@users.noreply.github.com>
2023-02-17 21:39:01 +0500

    Update MergeSort.java

--- Sorting/MergeSort.java
+++ Sorting/MergeSort.java
@@ -1,1 +1,50 @@
-//todo: 
+import java.util.Arrays;
+
+public class MergeSortRecursive {
+       public static void main(String[] args) {
+        int[] arr={8,3,4,12,5,6};
+        int[] ans=mergeSort(arr);
+        System.out.println(Arrays.toString(ans));
+       }
+
+       static int[] mergeSort(int[] arr ){
+        if(arr.length==1){
+            return arr;
+        }
+        // divide array into two half
+        int mid=arr.length/2;
+        int[] left=mergeSort(Arrays.copyOfRange(arr, 0, mid));
+        int[] right=mergeSort(Arrays.copyOfRange(arr, mid, arr.length));
+       return merge(left,right);
+       }
+
+    private static int[] merge(int[] first, int[] second) {
+        int[] mix=new int[first.length+second.length];
+        int i=0;
+        int j=0;
+        int k=0;
+        while (i< first.length && j <second.length) {
+                  if(first[i]<second[j]){
+                    mix[k]=first[i];
+                    i++;
+                  }else{
+                    mix[k]=second[j];
+                    j++; 
+                  }
+                  k++;
+        }
+        //it may be possible that one of array is not complete 
+  while(i<first.length){
+    mix[k]=first[i];
+    i++;
+    k++;
+  }
+  while(j<second.length){
+    mix[k]=second[j];
+    j++;
+    k++;
+  }
+  return mix;
+    }
+}
+
